d7c3eaac83a6d665256d16b59e810da07164f6c0
Matheus Maidel <matheus.maidel@gmail.com>
2025-01-26 17:09:26 -0300

    Basic parser design.

--- jsan.h
+++ jsan.h
@@ -1,10 +1,40 @@
 #ifndef JSAN_H
 #define JSAN_H
 
+#include <stddef.h>
+
+enum Jsan_Type {
+	JSAN_NULL,
+	JSAN_FALSE,
+	JSAN_TRUE,
+	JSAN_NUMBER,
+	JSAN_STRING,
+	JSAN_ARRAY,
+	JSAN_OBJECT,
+};
+
+struct Jsan {
+	const char *key;
+	union {
+		double number;
+		char *string;
+		struct Jsan *children;
+	} value;
+	size_t length;
+	enum Jsan_Type type;
+};
+
+struct Jsan *jsan_parse(const char *data, size_t size);
+void jsan_free(struct Jsan *root);
+void jsan_print(const struct Jsan *node);
+
 #endif // JSAN_H
 #ifdef JSAN_IMPL
 
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 struct Jsan_Arena {
 	char *begin;
@@ -42,12 +72,13 @@ jsan_arena_push(struct Jsan_Arena *arena, size_t size)
 		char *result = arena->current;
 		arena->current += size;
 		return result;
-	} else if (!arena->next) {
+	}
+	if (!arena->next) {
 		size_t this_size = arena->end - arena->begin;
-		arena->next = jsan_arena_create(size > this_size ? size : this_size);
+		arena->next = jsan_arena_create( (size > this_size) ? size : this_size);
 		if (!arena->next)
 			return NULL;
-	}
+	}	
 	return jsan_arena_push(arena->next, size);
 }
 
@@ -59,6 +90,171 @@ jsan_arena_pop(struct Jsan_Arena *arena, size_t size)
 	else
 		arena->current = arena->begin;
 	return arena->current;
+}
+
+struct Jsan_Parser {
+	const char *begin;
+	const char *current;
+	const char *end;
+	struct Jsan_Arena *arena;
+	struct Jsan *node;
+};
+
+static struct Jsan_Parser
+jsan_parser_make(const char *data, size_t size)
+{
+	struct Jsan_Parser parser = {0};
+	parser.begin = data;
+	parser.current = parser.begin;
+	parser.end = parser.begin + size;
+
+	size_t arena_size = sizeof (struct Jsan) * 1024;
+	parser.arena = jsan_arena_create(arena_size);
+	if (parser.arena)
+		parser.node = jsan_arena_push(parser.arena, sizeof (struct Jsan) );
+	
+	return parser;
+}
+
+static int
+jsan_parser_try_key(struct Jsan_Parser *parser)
+{
+	return -1;
+}
+
+static int
+jsan_parser_try_number(struct Jsan_Parser *parser)
+{
+	return -1;
+}
+
+static int
+jsan_parser_try_string(struct Jsan_Parser *parser)
+{
+	return -1;
+}
+
+static int
+jsan_parser_try_array(struct Jsan_Parser *parser)
+{
+	return -1;
+}
+
+static int
+jsan_parser_try_object(struct Jsan_Parser *parser)
+{
+	return -1;
+}
+
+static void
+jsan_parser_trim(struct Jsan_Parser *parser)
+{
+	while ( ( parser->current < parser->end )
+		&& isspace(*parser->current) )
+		parser->current++;
+}
+
+static int
+jsan_parser_try_value(struct Jsan_Parser *parser)
+{
+	size_t spare = parser->end - parser->current;
+	switch (spare) {
+	default:
+	case 5:
+		if (strncmp(parser->current, "false", 5) == 0) {
+			parser->node->type = JSAN_FALSE;
+			return 0;
+		}
+		
+	case 4:
+		if (strncmp(parser->current, "true", 4) == 0) {
+			parser->node->type = JSAN_TRUE;
+			return 0;
+		}
+		if (strncmp(parser->current, "null", 4) == 0) {
+			parser->node->type = JSAN_NULL;
+			return 0;
+		}
+
+	case 3:
+	case 2:
+	case 1:
+		if ( ('0' <= *parser->current) && (*parser->current <= '9') ) {
+			parser->node->type = JSAN_NUMBER;
+			return jsan_parser_try_number(parser);
+		}
+		if ( (*parser->current == '"') ) {
+			parser->node->type = JSAN_STRING;
+			return jsan_parser_try_string(parser);
+		}
+		if ( (*parser->current == '[') ) {
+			parser->node->type = JSAN_ARRAY; 
+			return jsan_parser_try_array(parser);
+		}
+		if ( (*parser->current == '{') ) {
+			parser->node->type = JSAN_OBJECT;
+			return jsan_parser_try_object(parser);
+		}
+		
+	case 0:
+		return -1;
+	}
+}
+
+struct Jsan *
+jsan_parse(const char *data, size_t size)
+{
+	struct Jsan_Parser parser = jsan_parser_make(data, size);
+	if (!parser.node)
+		return NULL;
+
+	struct Jsan *root = parser.node;
+	if (jsan_parser_try_value(&parser) != 0) {
+		jsan_arena_destroy(parser.arena);
+		return NULL;
+	}
+
+	return root;
+}
+
+void
+jsan_free(struct Jsan *root)
+{
+	free((char *) root - sizeof (struct Jsan_Arena));
+}
+
+// TODO: Fill a buffer instead of directly printing to stdout.
+void
+jsan_print(const struct Jsan *node)
+{
+	if (!node)
+		printf("---");
+		
+	switch (node->type) {
+	case JSAN_NULL:
+		printf("null");
+		break;
+	case JSAN_FALSE:
+		printf("false");
+		break;
+	case JSAN_TRUE:
+		printf("true");
+		break;
+	case JSAN_NUMBER:
+		printf("%f", node->value.number);
+		break;
+	case JSAN_STRING:
+		printf("%s", node->value.string);
+		break;
+	case JSAN_ARRAY:
+		printf("[%zu elements]", node->length);
+		break;
+	case JSAN_OBJECT:
+		printf("{%zu keys}", node->length);
+		break;
+	default:
+		printf("---");
+	}
 }
 
 #endif // JSAN_IMPL
---
+++ test_parser.c
@@ -0,0 +1,69 @@
+#define JSAN_IMPL
+#include "jsan.h"
+
+#define JSAN_TEST_PRINT(NAME, DATA) \
+	do { \
+		printf("Parsing " NAME "\n\t>>> "); \
+		const char *data = DATA; \
+		struct Jsan *node = jsan_parse(data, strlen(data) ); \
+		if (node) { \
+			jsan_print(node); \
+			printf("\n"); \
+			jsan_free(node); \
+		} else { \
+			printf("Failed to parse " NAME "\n"); \
+			return -1; \
+		} \
+	} while (0)
+
+int
+main(void)
+{
+	JSAN_TEST_PRINT(
+		"Depth 0 null",
+		"null");
+	JSAN_TEST_PRINT(
+		"Depth 0 false",
+		"false");
+	JSAN_TEST_PRINT(
+		"Depth 0 true",
+		"true");
+	JSAN_TEST_PRINT(
+		"Depth 0 number #0",
+		"42");
+	JSAN_TEST_PRINT(
+		"Depth 0 number #1 (float)",
+		"42.69");
+	JSAN_TEST_PRINT(
+		"Depth 0 number #2 (negative)",
+		"-123");
+	JSAN_TEST_PRINT(
+		"Depth 0 number #3 (float, negative)",
+		"-123.456");
+	JSAN_TEST_PRINT(
+		"Depth 0 string #0",
+		"\"abcdefg\"");
+	JSAN_TEST_PRINT(
+		"Depth 0 string #1 (empty)",
+		"\"\"");
+	JSAN_TEST_PRINT(
+		"Depth 0 array #0",
+		"\[0, 1, 2, 3]");
+	JSAN_TEST_PRINT(
+		"Depth 0 array #1 (empty)",
+		"\[]");
+	JSAN_TEST_PRINT(
+		"Depth 0 array #2 (different children types)",
+		"\[null, false, true, -123.456, \"abcdef\"]");
+	JSAN_TEST_PRINT(
+		"Depth 0 object",
+		"{\n"
+		"\t\"first\": null,\n"
+		"\t\"second\": false,\n"
+		"\t\"third\": true,\n"
+		"\t\"fourth\": -123.456,\n"
+		"\t\"fifth\": \"abcdef\"\n"
+		"}");
+
+	return 0;
+}
